ffcc989078574b357c0a52baccc944cdbaa5c182
Teoh Jie Yie <jieyiet2003@gmail.com>
2023-10-23 22:16:55 +0800

    first commit

---
+++ routes/instructors.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+
+// instruction: import the book model
+const Instructor = require("../models/instructor");
+
+// instruction: GET /: List all instructors
+router.get("/", async (req, res) => {
+  try {
+    res.status(200).send(await Instructor.find());
+  } catch (error) {
+    res.status(400).send({ message: "Instructor not found" });
+  }
+});
+
+// instruction: setup GET /:id: Get a specific instructor  by its _id
+router.get("/:id", async (req, res) => {
+  try {
+    const data = await Instructor.findOne({ _id: req.params.id });
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(400).send({ message: "Instructor id not found" });
+  }
+});
+
+// instruction: setup POST /: Add a new instructor
+router.post("/", async (req, res) => {
+  try {
+    const newInstructor = new Instructor({
+      name: req.body.name,
+      qualification: req.body.qualification,
+      profile: req.body.profile,
+      coursesTaught: req.body.coursesTaught,
+    });
+    await newInstructor.save();
+    res.status(200).send(newInstructor);
+  } catch (error) {
+    res.status(400).send({ message: error._message });
+  }
+});
+
+// instruction: setup PUT /:id: Update a instructor by its _id
+router.put("/:id", async (req, res) => {
+  try {
+    const instructor_id = req.params.id;
+
+    const updateInstructor = await Instructor.findByIdAndUpdate(
+      instructor_id,
+      req.body,
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+    res.status(200).send(updateInstructor);
+  } catch (error) {
+    res.status(400).send({ message: error._message });
+  }
+});
+
+// instruction: setup DELETE /:id: Delete a instructor by its _id
+router.delete("/:id", async (req, res) => {
+  try {
+    const instructor_id = req.params.id;
+    const deleteInstructor = await Instructor.findByIdAndDelete(instructor_id);
+    res.status(200).send(deleteInstructor);
+  } catch (error) {
+    res.status(400).send({ message: error._message });
+  }
+});
+
+// instruction: export the router
+module.exports = router;
